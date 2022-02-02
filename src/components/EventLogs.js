2b05865dc15a32492cf425a118fe23e7278030fc
Paul Armstrong <paulbarmstrong@hotmail.com>
2022-02-01 23:43:21 -0500

    Revert "add loading indicator for the event logs"
    This reverts commit 493c14c85ac32add3b294a7423924f8052f998c4.

--- src/components/EventLogs.js
+++ src/components/EventLogs.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import LoadingWheel from './LoadingWheel';
 
 class EventLogs extends React.Component {
 	constructor() {
 		super()
 		this.state = {
+			events: []
 		}
 		this.fetchData = this.fetchData.bind(this)
 	}
@@ -25,15 +25,11 @@ class EventLogs extends React.Component {
 				</thead>
 				<tbody>
 					{
-						this.state.events ? (
-							this.state.events.map(event => 
-								<tr>
-									<td>{this.getDateString(new Date(event.timestamp))}</td>
-									<td>{event.message}</td>
-								</tr>
-							)
-						) : (
-							<LoadingWheel/>
+						this.state.events.map(event => 
+							<tr>
+								<td>{this.getDateString(new Date(event.timestamp))}</td>
+								<td>{event.message}</td>
+							</tr>
 						)
 					}
 				</tbody>
